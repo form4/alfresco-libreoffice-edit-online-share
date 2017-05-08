22be5a0ab824d2639bc798a3d34d9825e529c0df
micha <micha@octopussy.fritz.box>
2017-05-08 23:23:03 +0200

    add localhost support

--- src/main/resources/META-INF/resources/components/documentlibrary/edit-online-libreoffice-action.js
+++ src/main/resources/META-INF/resources/components/documentlibrary/edit-online-libreoffice-action.js
@@ -12,8 +12,22 @@ YAHOO.Bubbling.fire("registerAction", {
     };
 
     var tmpHostname = window.location.href;
-    var regExp = /[\/\/\da-z\.-]+\.[a-z\.]{2,6}/;
-    var hostname = regExp.exec(tmpHostname);
+    var hostname = null;
+    
+    var regExpLocal = /^https?:\/\/localhost(:\d+).*?/;
+    if(regExpLocal.test(tmpHostname)) {
+    	console.log('localhost');
+    	hostname = regExpLocal.exec(tmpHostname)[0];
+    } else {
+    	console.log('domain');
+	    var regExp = /[\/\/\da-z\.-]+\.[a-z\.]{2,6}/;
+	    hostname = regExp.exec(tmpHostname);
+    }
+    
+    if(hostname == null) {
+    	console.error('Hostname could not be parsed oout of', tmpHostname);
+    	return;
+    }
 
     // Build the path from the WebDAV path: It already has any items with spaces or other characters url encoded
     var path = node.webdavUrl.replace("/webdav", hostname + "@SSL\/DavWWWRoot\/alfresco\/aos");
@@ -21,6 +35,7 @@ YAHOO.Bubbling.fire("registerAction", {
       // IE needs to double encode it.
       path = path.split("/").map(encodeURIComponent).join("/");
     }
+    console.log(hostname, 'open', path);
     window.location.href = "libreoffice:" + path;
   }
 });
